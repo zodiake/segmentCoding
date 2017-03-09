c25e84ecfdff217aeb7fcfdc91bcbd0470d49e6b
zodiake <qirong wang@nielsen.com>
2017-03-09 10:31:56 +0800

    add jd sum statics

---
+++ src/main/scala/com/nielsen/statics/JDstatics.scala
@@ -0,0 +1,25 @@
+package com.nielsen.statics
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by zodiake on 17-3-9.
+  */
+object JDstatics {
+  def main(args: Array[String]): Unit = {
+    val Seq(par, source, target) = args.toSeq
+    val conf = new SparkConf()
+    if (par == "dev") {
+      conf.setMaster("local[*]")
+      conf.setAppName("statics")
+    }
+    val sc = new SparkContext(conf)
+    val result = sc.textFile(source)
+      .map(_.split("\t"))
+      .map(i => (i(8), i(10).toDouble))
+      .groupByKey()
+      .map { i => (i._1, i._2.size, i._2.sum) }
+      .map(i => (i._1, i._2, f"${i._3}%.2f"))
+    result.saveAsTextFile(target)
+  }
+}
