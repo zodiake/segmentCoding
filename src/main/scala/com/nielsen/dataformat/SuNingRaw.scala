166d6e9df235f8ccbf60eb9383135a6a94d74dd2
zodiake <qirong wang@nielsen.com>
2017-06-06 11:17:43 +0800

    change suning format logic delete * 10000

--- src/main/scala/com/nielsen/dataformat/SuNingRaw.scala
+++ src/main/scala/com/nielsen/dataformat/SuNingRaw.scala
@@ -21,7 +21,7 @@ case class SuNingRaw(storeId: String,
 
   def toCsvString(year: String): String = {
     val storeCode = if (pay != "0") "20270" else "20271"
-    val unitPrice = if (salesVolumn == 0) 0 else salesValue * 10000 / salesVolumn
+    val unitPrice = if (salesVolumn == 0) 0 else salesValue / salesVolumn
     //storeId + "," + "\"" + city + "\"" + "," + itemCode + "," + storeCode + "," + "SUNING" + "," + prodCate.replace(",", "") + "," + EAN + "," + "" + "," + "" + "," + "\"" + "\"" + "," + "\"" + "\"" + "," + "" + "," + "\"" + desc.replace(",", "") + "\"" + "," + salesValue * 10000 + "," + 1 + "," + salesVolumn + "," + payValue * 10000 + "," + payValue * 10000 + "," + year.substring(0, 4) + "-" + year.substring(6) + "-" + "01" + "," + "\"" + " " + "\"" + "," + ""
     val newFormat =
       s"""${storeId},"${city}",${itemCode},${storeCode},SUNING,${prodCate.replace(",", "")},${EAN},,,"","",,"${desc.replace(",", "")}",${unitPrice},1,${salesVolumn},${payValue * 10000},${unitPrice},${year.substring(0, 4)}-${year.substring(6)}-01," ", """
